18b031329396eacf655c3a611c72c92c384cb178
yosumkmk <hi.spread@gmail.com>
2019-03-19 08:41:23 +0900

    0319 1st commit  uoooooooo

---
+++ data/storage/src_2019-03-18_2217_qwk_0481/data/make_dataset.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import warnings
+from src.util.log_util import set_logger
+import pandas as pd
+warnings.filterwarnings('ignore')
+from logging import StreamHandler, Formatter, getLogger, FileHandler, DEBUG, INFO, ERROR
+import numpy as np
+import os
+import data.input
+from keras.applications.densenet import preprocess_input, DenseNet121
+
+import cv2
+
+
+logger = set_logger(__name__)
+
+
+img_size = 256
+batch_size = 256
+
+
+def resize_to_square(im):
+    old_size = im.shape[:2]
+    ratio = float(img_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    im = cv2.resize(im, (new_size[1], new_size[0]))
+    delta_w = img_size - new_size[1]
+    delta_h = img_size - new_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+    color = [0, 0, 0]
+    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    return new_im
+
+
+def load_image(path, pet_id):
+    image = cv2.imread(f'{path}{pet_id}-1.jpg')
+    new_image = resize_to_square(image)
+    new_image = preprocess_input(new_image)
+    return new_image
+
+
+
+
+
+
+
+#############
+def read_train_data(path=data.input.__path__[0] ,nrows=None):
+    logger.info('Input train_data')
+    train_df = pd.read_csv(os.path.join(path, 'train.csv'), nrows=nrows)
+    return train_df
+
+def read_test_data(path=data.input.__path__[0]):
+    logger.info('Input test_data')
+    test_df = pd.read_csv(os.path.join(path, 'test.csv'))
+    return test_df
+
+def read_permutation_importance(name='0303_base_feature.csv'):
+    return pd.read_csv(os.path.join(data.permutation_importance.__path__[0], name))
+
+def split_train_data(train, split_rate):
+    split_index = len(train) // split_rate
+    return train[0:split_index], train[split_index]
+
+def check_missing_data(df):
+    flag=df.isna().sum().any()
+    if flag==True:
+        total = df.isnull().sum()
+        percent = (df.isnull().sum())/(df.isnull().count()*100)
+        output = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+        data_type = []
+        # written by MJ Bahmani
+        for col in df.columns:
+            dtype = str(df[col].dtype)
+            data_type.append(dtype)
+        output['Types'] = data_type
+        return(np.transpose(output))
+    else:
+        return(False)
+
+if __name__ == '__main__':
+    pass
---
+++ src/features/word_parse.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import glob
+from src.util.log_util import set_logger
+from logging import StreamHandler, Formatter, getLogger, FileHandler, DEBUG, INFO, ERROR
+from sklearn.preprocessing import StandardScaler
+from src.data.make_dataset import read_permutation_importance
+from sklearn.decomposition import TruncatedSVD
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from data import input
+import os
+
+logger = set_logger(__name__)
+
+
+def parse_tfidf(X_temp, X_text):
+    n_components = 16
+    text_features = []
+
+    # Generate text features:
+    for i in X_text.columns:
+        # Initialize decomposition methods:
+        print(f'generating features from: {i}')
+        tfv = TfidfVectorizer(min_df=2, max_features=None,
+                              strip_accents='unicode', analyzer='word', token_pattern=r'(?u)\b\w+\b',
+                              ngram_range=(1, 3), use_idf=1, smooth_idf=1, sublinear_tf=1)
+        svd_ = TruncatedSVD(
+            n_components=n_components, random_state=1337)
+
+        tfidf_col = tfv.fit_transform(X_text.loc[:, i].values)
+
+        svd_col = svd_.fit_transform(tfidf_col)
+        svd_col = pd.DataFrame(svd_col)
+        svd_col = svd_col.add_prefix('TFIDF_{}_'.format(i))
+
+        text_features.append(svd_col)
+
+    text_features = pd.concat(text_features, axis=1)
+
+    X_temp = pd.concat([X_temp, text_features], axis=1)
+
+    for i in X_text.columns:
+        X_temp = X_temp.drop(i, axis=1)
+    return X_temp
+
+class PetFinderParser(object):
+
+    def __init__(self, debug=False):
+
+        self.debug = debug
+        self.sentence_sep = ' '
+
+        self.extract_sentiment_text = False
+
+    def open_json_file(self, filename):
+        with open(filename, 'r', encoding='utf-8') as f:
+            json_file = json.load(f)
+        return json_file
+
+    def parse_sentiment_file(self, file):
+        """
+        Parse sentiment file. Output DF with sentiment features.
+        """
+
+        file_sentiment = file['documentSentiment']
+        file_entities = [x['name'] for x in file['entities']]
+        file_entities = self.sentence_sep.join(file_entities)
+
+        file_sentences_sentiment = [x['sentiment'] for x in file['sentences']]
+
+        file_sentences_sentiment = pd.DataFrame.from_dict(
+            file_sentences_sentiment, orient='columns')
+        file_sentences_sentiment_df = pd.DataFrame(
+            {
+                'magnitude_sum': file_sentences_sentiment['magnitude'].sum(axis=0),
+                'score_sum': file_sentences_sentiment['score'].sum(axis=0),
+                'magnitude_mean': file_sentences_sentiment['magnitude'].mean(axis=0),
+                'score_mean': file_sentences_sentiment['score'].mean(axis=0),
+                'magnitude_var': file_sentences_sentiment['magnitude'].var(axis=0),
+                'score_var': file_sentences_sentiment['score'].var(axis=0),
+            }, index=[0]
+        )
+
+        df_sentiment = pd.DataFrame.from_dict(file_sentiment, orient='index').T
+        df_sentiment = pd.concat([df_sentiment, file_sentences_sentiment_df], axis=1)
+
+        df_sentiment['entities'] = file_entities
+        df_sentiment = df_sentiment.add_prefix('sentiment_')
+
+        return df_sentiment
+
+    def parse_metadata_file(self, file):
+        """
+        Parse metadata file. Output DF with metadata features.
+        """
+
+        file_keys = list(file.keys())
+
+        if 'labelAnnotations' in file_keys:
+            file_annots = file['labelAnnotations']
+            file_top_score = np.asarray([x['score'] for x in file_annots]).mean()
+            file_top_desc = [x['description'] for x in file_annots]
+        else:
+            file_top_score = np.nan
+            file_top_desc = ['']
+
+        file_colors = file['imagePropertiesAnnotation']['dominantColors']['colors']
+        file_crops = file['cropHintsAnnotation']['cropHints']
+
+        file_color_score = np.asarray([x['score'] for x in file_colors]).mean()
+        file_color_pixelfrac = np.asarray([x['pixelFraction'] for x in file_colors]).mean()
+
+        file_crop_conf = np.asarray([x['confidence'] for x in file_crops]).mean()
+
+        if 'importanceFraction' in file_crops[0].keys():
+            file_crop_importance = np.asarray([x['importanceFraction'] for x in file_crops]).mean()
+        else:
+            file_crop_importance = np.nan
+
+        df_metadata = {
+            'annots_score': file_top_score,
+            'color_score': file_color_score,
+            'color_pixelfrac': file_color_pixelfrac,
+            'crop_conf': file_crop_conf,
+            'crop_importance': file_crop_importance,
+            'annots_top_desc': self.sentence_sep.join(file_top_desc)
+        }
+
+        df_metadata = pd.DataFrame.from_dict(df_metadata, orient='index').T
+        df_metadata = df_metadata.add_prefix('metadata_')
+
+        return df_metadata
+
+def extract_additional_features(pet_id, mode='train'):
+    pet_parser = PetFinderParser()
+    sentiment_filename = os.path.join(input.__path__[0],
+                                      f'petfinder-adoption-prediction/{mode}_sentiment/{pet_id}.json')
+    try:
+        sentiment_file = pet_parser.open_json_file(sentiment_filename)
+        df_sentiment = pet_parser.parse_sentiment_file(sentiment_file)
+        df_sentiment['PetID'] = pet_id
+    except FileNotFoundError:
+        df_sentiment = []
+
+    dfs_metadata = []
+    metadata_filenames = sorted(glob.glob(os.path.join(input.__path__[0],
+                                                       f'petfinder-adoption-prediction/{mode}_metadata/{pet_id}*.json')))
+    if len(metadata_filenames) > 0:
+        for f in metadata_filenames:
+            metadata_file = pet_parser.open_json_file(f)
+            df_metadata = pet_parser.parse_metadata_file(metadata_file)
+            df_metadata['PetID'] = pet_id
+            dfs_metadata.append(df_metadata)
+        dfs_metadata = pd.concat(dfs_metadata, ignore_index=True, sort=False)
+    dfs = [df_sentiment, dfs_metadata]
+
+    return dfs
+
+if __name__ == '__main__':
+    pass
